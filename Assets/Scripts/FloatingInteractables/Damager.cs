51d330d8cd09bd23adbbb8752d1e403200c224db
Artur <Artur@email.com>
2024-05-24 16:36:06 +0300

    +

--- Assets/Scripts/FloatingInteractables/Damager.cs
+++ Assets/Scripts/FloatingInteractables/Damager.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class Damager : MonoBehaviour
 {
+	[SerializeField] private bool _destroyOnHit = true;
+
 	private void OnCollisionEnter(Collision collision)
 	{
 		if (collision.gameObject.TryGetComponent(out Health health))
 		{
 			health.Hit();
+
+			if (_destroyOnHit)
+				Destroy(gameObject);
 		}
 	}
 }

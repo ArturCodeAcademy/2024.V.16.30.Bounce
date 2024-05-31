2b081ca9e0f51e9f86bbcdb43de2d5c517b3b357
ArturGimbarr75 <123232198+ArturCodeAcademy@users.noreply.github.com>
2024-05-31 17:57:58 +0300

    +

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	[SerializeField] private RectTransform _content;
+	[SerializeField] private Button _buttonPrefab;
+
+	private void Start()
+	{
+		int levelCount = SceneManager.sceneCountInBuildSettings;
+
+        for (int i = 1; i < levelCount; i++)
+        {
+			var button = Instantiate(_buttonPrefab, _content);
+			TMP_Text levelText = button.transform.GetChild(0).GetComponent<TMP_Text>();
+			levelText.text = $"Level {i}";
+
+			TMP_Text recordText = button.transform.GetChild(1).GetComponent<TMP_Text>();
+			string sceneName = SceneUtility.GetScenePathByBuildIndex(i) + "_record";
+			int record = PlayerPrefs.GetInt(sceneName, 0);
+			if (record > 0)
+				recordText.text = $"Best time:\n{record/60}:{record%60}";
+			else
+				recordText.text = "No record";
+
+			int levelIndex = i;
+			button.onClick.AddListener(() => SceneManager.LoadScene(levelIndex));
+
+			button.gameObject.SetActive(true);
+		}
+    } 
+
+	public void ExitGame()
+    {
+		Application.Quit();
+	}
+}
